c268bb4d911030cb05e44806d328871f624487dc
Marisa Clardy <marisa@clardy.eu>
2019-12-07 15:42:26 -0600

    Add empty testcase test.

--- tests/JsonConverterTest.php
+++ tests/JsonConverterTest.php
@@ -13,8 +13,19 @@ class JsonConverterTest extends TestCase
         $json = file_get_contents(__DIR__.'/example-input.json');
         $converter = new JsonConverter($json);
         $xml = $converter->getXML();
-        $this->assertIsString($xml);
+
         $this->assertStringContainsString('xsi:noNamespaceSchemaLocation', $xml);
         $this->assertStringEqualsFile(__DIR__.'/example-output.xml', $xml);
+    }
+
+    /** @test */
+    function it_creates_an_empty_report_on_success()
+    {
+        $json = '[]';
+        $converter = new JsonConverter($json);
+        $xml = $converter->getXML();
+
+        $this->assertIsString($xml);
+        $this->assertStringContainsString('<testcase name="psalm"/>', $xml);
     }
 }
